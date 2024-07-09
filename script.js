d10b6481431ab2247ef61af91bee988c0bc188e9
kris3580 <krisssss3580@gmail.com>
2024-07-09 22:30:36 +0300

    more gifs and compressed even moreee

--- script.js
+++ script.js
@@ -14,3 +14,37 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const typeSelect = document.getElementById('typeSelect');
+    const toolSelect = document.getElementById('toolSelect');
+
+    function filterImages() {
+        const selectedType = typeSelect.value;
+        const selectedTool = toolSelect.value;
+
+        const thumbnails = document.querySelectorAll('.thumbnail');
+        thumbnails.forEach(img => {
+            const imgTypes = img.getAttribute('data-type').split(',').map(type => type.trim());
+            const imgTools = img.getAttribute('data-tool').split(',').map(tool => tool.trim());
+            const imgParent = img.parentNode;
+
+            let typeMatch = selectedType === 'All' || imgTypes.includes(selectedType);
+            let toolMatch = selectedTool === 'All' || imgTools.includes(selectedTool);
+
+            if (typeMatch && toolMatch) {
+                imgParent.style.display = 'block';
+                //imgParent.removeAttribute('href');
+            } else {
+                imgParent.style.display = 'none';
+                //imgParent.setAttribute('href', '#');
+            }
+        });
+    }
+
+    typeSelect.addEventListener('change', filterImages);
+    toolSelect.addEventListener('change', filterImages);
+
+    filterImages();
+});
